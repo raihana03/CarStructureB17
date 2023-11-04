e0001970a7339742da0db54869532a3e8ff6f576
raihana03 <raihana.abbasi03@gmail.com>
2023-11-04 12:18:56 -0400

    i have added the feature lights

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -6,5 +6,11 @@ public class main {
         audi.Hp="5000";
         audi.Doors="2";
         audi.Model="A90";
+
+        Lights light=new Lights();
+        light.color="blue";
+        light.opacity="50";
+        light.size="40'";
+
     }
 }
